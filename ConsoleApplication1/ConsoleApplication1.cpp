f6870bed0613131bf4132b80cae2e0ef3b8b0a63
lyj <lyj8512@126.com>
2021-03-14 23:33:38 +0800

    90%

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,13 +8,18 @@ int main()
 #pragma region(S) init set
 	IloEnv env;
 	IloModel model(env);
-	IloNumVarArray call_x(env), put_x(env);
-	IloNumVarArray put_isBid(env), put_isAsk(env);
+	IloNumVarArray call_x_p(env), put_x_p(env);
+	IloNumVarArray call_x_n(env), put_x_n(env);
+
+
+
 
 	for (size_t i = 0; i < 7; i++)
 	{
-		call_x.add(IloNumVar(env, -0, IloInfinity, ILOFLOAT));
-		put_x.add(IloNumVar(env, -0, IloInfinity, ILOFLOAT));
+		call_x_p.add(IloNumVar(env, 0, IloInfinity, ILOFLOAT));
+		put_x_p.add(IloNumVar(env, 0, IloInfinity, ILOFLOAT));		
+		call_x_n.add(IloNumVar(env, -10, 0, ILOFLOAT));
+		put_x_n.add(IloNumVar(env, -10, 0, ILOFLOAT));
 
 	}
 
@@ -26,8 +31,8 @@ int main()
 
 	int num = 7;
 	IloIntArray k(env, num);
-	IloNumArray call_Bid(env, num),call_Ask(env, num),call_Expected(env, num);
-	IloNumArray put_Bid(env, num),put_Ask(env, num),put_Expected(env, num);
+	IloNumArray call_Bid(env, num), call_Ask(env, num), call_Expected(env, num);
+	IloNumArray put_Bid(env, num), put_Ask(env, num), put_Expected(env, num);
 
 	IloNum S_0 = 760.48;
 	IloNum S_T = 765.32;
@@ -59,7 +64,7 @@ int main()
 	// read call option data
 	for (int i = 0; i < 7; i++)
 	{
-		fscanf_s(input_para, "%s", &tempstr,10);
+		fscanf_s(input_para, "%s", &tempstr, 10);
 		fscanf_s(input_para, "%d", &k[i]);
 		fscanf_s(input_para, "%lf", &call_Bid[i]);
 		fscanf_s(input_para, "%lf", &tempnum);
@@ -68,7 +73,7 @@ int main()
 		fscanf_s(input_para, "%lf", &call_Expected[i]);
 		fscanf_s(input_para, "%lf", &tempnum);
 		fscanf_s(input_para, "%lf", &tempnum);
-		//env.out() << k[i] << "," << call_Bid[i] << "," << call_Ask[i] << "," << call_Expected[i] << endl;
+		env.out() << "C" << k[i] << "\t" << call_Bid[i] << "\t" << call_Ask[i] << "\t" << call_Expected[i] << endl;
 
 	}
 
@@ -76,7 +81,7 @@ int main()
 	// read put option data
 	for (size_t i = 0; i < 7; i++)
 	{
-		fscanf_s(input_para, "%s", &tempstr,10);
+		fscanf_s(input_para, "%s", &tempstr, 10);
 		fscanf_s(input_para, "%d", &k[i]);
 		fscanf_s(input_para, "%lf", &put_Bid[i]);
 		fscanf_s(input_para, "%lf", &tempnum);
@@ -85,13 +90,13 @@ int main()
 		fscanf_s(input_para, "%lf", &put_Expected[i]);
 		fscanf_s(input_para, "%lf", &tempnum);
 		fscanf_s(input_para, "%lf", &tempnum);
-		//env.out() << k[i] <<"," << put_Bid[i]<< "," << put_Ask[i] << "," << put_Expected[i] << endl;
+		env.out() << "P" << k[i] << "\t" << put_Bid[i] << "\t" << put_Ask[i] << "\t" << put_Expected[i] << endl;
 
 	}
 	//system("pause");
 
 	std::fclose(input_para);
-	 
+
 #pragma endregion(E)
 
 
@@ -99,7 +104,8 @@ int main()
 
 	IloNumExpr sum_Exp(env);
 	for (int i = 0; i < 7; i++) {
-		sum_Exp += (call_x[i] * call_Expected[i] * 1 + put_x[i] * put_Expected[i]);
+		sum_Exp += (call_x_p[i] * call_Expected[i] + put_x_p[i] * put_Expected[i]);
+		sum_Exp += (call_x_n[i] * call_Expected[i]  + put_x_n[i] * put_Expected[i]);
 	}
 
 	// Obj
@@ -119,61 +125,106 @@ int main()
 
 	IloNumExpr sum_Ask(env);
 	for (int i = 0; i < 7; i++) {
-		sum_Ask += (call_x[i] * call_Ask[i] * 1 + put_x[i] * put_Ask[i]);
+		sum_Ask += (call_x_p[i] * call_Ask[i]  + put_x_p[i] * put_Ask[i]);
 	}
 
 	IloNumExpr sum_Bid(env);
 	for (int i = 0; i < 7; i++) {
-		sum_Bid += (call_x[i] * call_Bid[i] * 1 + put_x[i] * put_Bid[i]);
+		sum_Bid += (call_x_n[i] * call_Bid[i]  + put_x_n[i] * put_Bid[i]);
 	}
 
 	// contraints (17)
-	IloAdd(model, sum_Ask + y * S_0 + z <= cash);
+	IloAdd(model, sum_Ask + sum_Bid + y * S_0 + z <= cash);
 
 
+	IloNumExprArray v_fun_exp_array(env);
 
-	IloNumExpr  V_fun_exp(env);
+	//创建一个表达式
+	v_fun_exp_array.add(IloNumExpr(env));
 	for (int i = 0; i < 7; i++) {
-		V_fun_exp += (put_x[i] * IloMax(0, (k[i] - S_T)) + call_x[i] * IloMax(0, (S_T - k[i])));
+		v_fun_exp_array[0] += (put_x_p[i] + put_x_n[i]) * IloMax(0, (k[i] - 0));
+		v_fun_exp_array[0] += (call_x_p[i] + call_x_n[i]) * IloMax(0, (0 - k[i]));
 	}
-	V_fun_exp += z * (1 + r) ;
-	
-	
+	v_fun_exp_array[0] += (z * (1 + r)+ y * (0+1.59));
+
 	// constraints (18)
-	IloAdd(model, V_fun_exp + y * 0 >= cash* theta);
-	
-	
+	IloAdd(model, v_fun_exp_array[0] >= cash * theta);
+
+
+
+
+
 	// constraints (19)
-	IloAdd(model, V_fun_exp + y * k[0] >= V_fun_exp + y * 0);	
-	
-	for (int i = 1; i < 6; i++) {
-		IloAdd(model, V_fun_exp + y* k[i + 1] >= V_fun_exp + y * k[i]);
+	IloInt t_k = k[0];
+	v_fun_exp_array.add(IloNumExpr(env));
+	for (int i = 0; i < 7; i++) {
+		v_fun_exp_array[1] += (put_x_p[i] + put_x_n[i]) * IloMax(0, (k[i] - t_k));
+		v_fun_exp_array[1] += (call_x_p[i] + call_x_n[i]) * IloMax(0, (t_k - k[i]));
 	}
-	//IloAdd(model, V_fun_exp + y *763 >=150);
-//
+	v_fun_exp_array[1] += (z * (1 + r) + y * (t_k + 1.59));
+	IloAdd(model, v_fun_exp_array[1] >= v_fun_exp_array[0]);
+
+
+
+
+
+
+
+
+	// constraints (20)
+	for (int j = 1; j <6; j++) {
+		t_k = k[j];
+		v_fun_exp_array.add(IloNumExpr(env));
+		for (int i = 0; i < 7; i++) {
+			v_fun_exp_array[j + 1] += (put_x_p[i] + put_x_n[i]) * IloMax(0, (k[i] - t_k));
+			v_fun_exp_array[j + 1] += (call_x_p[i] + call_x_n[i]) * IloMax(0, (t_k - k[i]));
+		}
+		v_fun_exp_array[j+1] += (z * (1 + r) + y * (t_k + 1.59));
+		IloAdd(model, v_fun_exp_array[j+1] >= v_fun_exp_array[j]);
+	}
+
+
+
+	
+	// constraints (20)
 	IloNumExpr  sum_callx(env);
 	for (int i = 0; i < 7; i++) {
-		sum_callx += call_x[i];
+		sum_callx += call_x_p[i];
 	}
 	IloAdd(model, y + sum_callx >= 0);
+	
+
+	
+	t_k = 760;
+	v_fun_exp_array.add(IloNumExpr(env));
+	for (int i = 0; i < 7; i++) {
+		v_fun_exp_array[7] += (put_x_p[i] + put_x_n[i]) * IloMax(0, (k[i] - t_k));
+		v_fun_exp_array[7] += (call_x_p[i] + call_x_n[i]) * IloMax(0, (t_k - k[i]));
+	}
+	v_fun_exp_array[7] += z * (1 + r) + y  * (t_k + 1.59);
+	IloAdd(model, v_fun_exp_array[7] >= 100);
 
 #pragma endregion
-
-
-
 	cpl.solve();
+	cpl.exportModel("model.lp");
+
+
+
+
+
+
 
 
 #pragma region (S) output optimal solution  
 
 	for (size_t i = 0; i < 7; i++)
 	{
-		printf("%s %d %s %lf %s", "Call-",k[i], ":\t",cpl.getValue(call_x[i]), "\n");
+		printf("C-%d\t%lf\t%lf\n", k[i], cpl.getValue(call_x_p[i]), cpl.getValue(call_x_n[i]));
 	}
 
 	for (size_t i = 0; i < 7; i++)
 	{
-		printf("%s %d %s %lf %s", "Put-", k[i], ":\t", cpl.getValue(put_x[i]), "\n");
+		printf("P-%d\t%lf\t%lf\n", k[i], cpl.getValue(put_x_p[i]), cpl.getValue(put_x_n[i]));
 	}
 
 	printf("%s %lf %s","y:", cpl.getValue(y),"\n");
